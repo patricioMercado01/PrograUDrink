d24f456fed90aabbba24ff3b03eb37999008896b
Jessx3x3 <j.vasquez12@ufromail.cl>
2017-10-31 16:23:37 -0300

    LOGIN + REGISTRO INCLUÍDOS EN FAVORITOS CAMBIÉ LOS NOMBRES DE LOS CÓCTELES, POR NOMBRES DECENTES XD

--- app/src/main/java/pmh/prograudrink/Favoritos.java
+++ app/src/main/java/pmh/prograudrink/Favoritos.java
@@ -1,5 +1,6 @@
 package pmh.prograudrink;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -18,21 +19,25 @@ public class Favoritos extends Fragment {
                              Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_favoritos,container,false);
 
+
+
+        Intent intent = new Intent(getActivity(), Login.class);
+        getActivity().startActivity(intent);
+
+
         String cocteles[] = new String[] {
-                "Aspirina tequila",
-                "Gelatina de salbutamol",
-                "mojito africano",
-                "Mojito de pollito",
-                "Cóctel de granizado tu hermana",
-                "Cóctel peruano",
-                "Sangría de caviar"};
+                "Terremoto chileno",
+                "Piscola",
+                "Blue moon",
+                "Michelada",
+                "Whisky rojo",
+                "Martini seco"};
 
         ListView listView = (ListView)view.findViewById(R.id.favoritos);
 
         ArrayAdapter<String> lisAdapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_expandable_list_item_1,cocteles);
         listView.setAdapter(lisAdapter);
-
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             FragmentManager fragmentManager = getFragmentManager();
@@ -42,14 +47,8 @@ public class Favoritos extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                  FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.Contenedor,detalleCoctel).addToBackStack(null).commit();
-
             }
         });
-
-
-
         return view;
     }
-
-
 }
---
+++ app/src/main/java/pmh/prograudrink/Login.java
@@ -0,0 +1,30 @@
+package pmh.prograudrink;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button btnRegistro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnRegistro = (Button)findViewById(R.id.btnRegistrar);
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Registro.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
