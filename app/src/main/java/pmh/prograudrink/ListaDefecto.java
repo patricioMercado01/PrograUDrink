f8afb94af044adb4efc4158653d04c01b468e002
patricioMercado01 <p.mercado01@ufromail.cl>
2017-11-14 17:58:05 -0300

    implementacion de database firebase, en las listas

--- app/src/main/java/pmh/prograudrink/ListaDefecto.java
+++ app/src/main/java/pmh/prograudrink/ListaDefecto.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,53 +12,77 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.content.ContentValues.TAG;
+
 
 public class ListaDefecto extends Fragment {
+    private FirebaseDatabase database;
+    private String COCTEL_CHILD = "cocteles";
+
+    private String cocteles;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_fragmen_list,container,false);
+        database = FirebaseDatabase.getInstance();
+        final DatabaseReference coctelReference = database.getReference().child(COCTEL_CHILD);
 
-        String cocteles[] = new String[] {
-                "Margarita corona",
-                "Gelatina de champaña",
-                "Margarita de fresa con tequila",
-                "Sorbete de piña al cava",
-                "Caipiroska de pepino",
-                "El trago de la bandera de colombia",
-                "Fresa espumante",
-                "Whiskey: Irish Redhead",
-                "Clericot de vino blanco",
-                "Carajillo de Bailey's",
-                "Sorbete de mojito",
-                "Trago de durazno",
-                "Blueberry Spritzer",
-                "Mojito de vodka con maracuyá",
-                "Cóctel de granizado de ginebra",
-                "Cóctel americano",
-                "Sangría de cava"};
-
-        ListView listView = (ListView)view.findViewById(R.id.listView);
-
-        ArrayAdapter<String> lisAdapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_expandable_list_item_1,cocteles);
-        listView.setAdapter(lisAdapter);
+        final ListView listView = (ListView)view.findViewById(R.id.listView);
 
 
+        coctelReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.d(TAG,"coctel listo"+ dataSnapshot.getValue().toString());
+                cocteles += (dataSnapshot.getValue().toString()+",");
 
-    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        FragmentManager fragmentManager = getFragmentManager();
-        DetalleCoctel detalleCoctel = new DetalleCoctel();
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-           if (i>=0 && i<=16){
-               FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
-               fragmentTransaction.replace(R.id.Contenedor,detalleCoctel).addToBackStack(null).commit();
-           }
-        }
-    });
+                ArrayAdapter<String> lisAdapter = new ArrayAdapter<String>(getActivity(),
+                        android.R.layout.simple_expandable_list_item_1,cocteles.split(","));
+                listView.setAdapter(lisAdapter);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            FragmentManager fragmentManager = getFragmentManager();
+            DetalleCoctel detalleCoctel = new DetalleCoctel();
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i>=0 && i<=16){
+                    FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.Contenedor,detalleCoctel).addToBackStack(null).commit();
+                }
+            }
+        });
 
         return view;
     }
