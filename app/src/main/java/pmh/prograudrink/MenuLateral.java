03f91f72d7f2576907607aebf3fc31d4358e361d
patricioMercado01 <p.mercado01@ufromail.cl>
2017-11-17 11:01:24 -0300

    log out

--- app/src/main/java/pmh/prograudrink/MenuLateral.java
+++ app/src/main/java/pmh/prograudrink/MenuLateral.java
@@ -2,6 +2,7 @@ package pmh.prograudrink;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
@@ -14,6 +15,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MenuLateral extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -37,6 +43,24 @@ public class MenuLateral extends AppCompatActivity
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.Contenedor,new ListaDefecto()).commit();
+
+        Button cerrarSesion = (Button) findViewById(R.id.logout);
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut() {
+        AuthUI.getInstance().signOut(MenuLateral.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                startActivity(new Intent(MenuLateral.this, LoginFB.class));
+                finish();
+            }
+        });
     }
 
     @Override
