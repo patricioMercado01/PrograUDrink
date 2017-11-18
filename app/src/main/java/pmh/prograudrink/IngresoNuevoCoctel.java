884ff66ffebe7478ceea302348bb63e6d3d35501
patricioMercado01 <p.mercado01@ufromail.cl>
2017-11-18 19:17:39 -0300

    Activity ingreso nuevo coctel

---
+++ app/src/main/java/pmh/prograudrink/IngresoNuevoCoctel.java
@@ -0,0 +1,13 @@
+package pmh.prograudrink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IngresoNuevoCoctel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingreso_nuevo_coctel);
+    }
+}
