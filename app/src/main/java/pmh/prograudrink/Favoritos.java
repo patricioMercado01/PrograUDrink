ca765ff2e52da7d483ff363e0db3ac862396d7e8
Jessx3x3 <j.vasquez12@ufromail.cl>
2017-11-20 19:35:02 -0300

    Prueba + firebase autentificación

--- app/src/main/java/pmh/prograudrink/Favoritos.java
+++ app/src/main/java/pmh/prograudrink/Favoritos.java
@@ -13,8 +13,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
